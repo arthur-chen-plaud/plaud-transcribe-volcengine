aa55fb2fc2ded56b6785187ad41917f402c582a8
chenye <chenye@users.noreply.github.com>
2026-06-01 16:15:38 +0800

    Remove mock integration mode

--- src/tasks.py
+++ src/tasks.py
@@ -3,7 +3,7 @@ import time
 from celery import Celery, signals
 from loguru import logger
 
-from config import BROKER_URL, HEALTH_PORT, QUEUE_NAME, RESULT_BACKEND, TASK_NAME, VOLCENGINE_MOCK_MODE
+from config import BROKER_URL, HEALTH_PORT, QUEUE_NAME, RESULT_BACKEND, TASK_NAME
 from src.datas import TranscribeRequest
 from src.health_server import start_health_server
 from src.secret_keys import start_volcengine_secretmanager_monitor
@@ -32,10 +32,7 @@ celery_app.conf.update(
 def worker_init_handler(**kwargs):
     logger.info("volcengine worker_init signal received")
     start_health_server(port=HEALTH_PORT)
-    if VOLCENGINE_MOCK_MODE:
-        logger.info("volcengine credential monitor skipped in mock mode")
-    else:
-        start_volcengine_secretmanager_monitor()
+    start_volcengine_secretmanager_monitor()
     warmUp()
     logger.info("volcengine worker_init signal end")
 
